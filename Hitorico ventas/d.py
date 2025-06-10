dbaa36da23eb0b48a11a83d4a16014d366bee34d
Sebastian Atia - Nutralmix SRL <sebastiana@nutralmix.com.ar>
2025-06-10 16:49:59 -0300

    Subo archivos de empleados

--- Hitorico ventas/d.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-import pandas as pd
-conn = sqlite3.connect("historial_precios.db")
-df = pd.read_sql_query("SELECT `Nombre del producto`, `Variacion 9dj`, `Variacion TL` FROM precios", conn)
-print(df.head(10))
-conn.close()
--- Hitorico ventas/exporta_auto.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd "C:\Programas Pyt\Hitorico ventas"
-env\Scripts\python.exe Ventas.py
-env\Scripts\python.exe exporta.py
