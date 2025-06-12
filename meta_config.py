8ab00dd45d9e97f1c0ff99eb106dee8cb7836c58
Sebastian Atia - Nutralmix SRL <sebastiana@nutralmix.com.ar>
2025-06-12 10:23:54 -0300

    Mejora búsqueda en subir_archivo_empleado y soporte a múltiples resultados

--- meta_config.py
+++ meta_config.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # Carga las variables de entorno desde .env
+load_dotenv()
 
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 API_VERSION = os.getenv("API_VERSION")
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+BASE_URL = os.getenv("BASE_URL", "https://whatsapp-bot-1wj3.onrender.com")
+
