6b95a14d317837b7f7df26bf82b844ae194a7b6b
unknown <SERVER-DM$@nutralmix.local>
2025-10-31 11:24:56 -0300

    Actualización automática - 2025-10-31 11:24

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import subprocess
 from flask import redirect
 import sys
 
-# para que stdout/stderr usen UTF-8 aunque Windows no lo tenga por defecto
 if hasattr(sys.stdout, "reconfigure"):
     sys.stdout.reconfigure(encoding="utf-8")
 if hasattr(sys.stderr, "reconfigure"):
